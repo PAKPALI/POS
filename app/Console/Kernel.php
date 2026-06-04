431531fc14826c36904e56e0811d385d395ab50c
PAKPALI <77837289+PAKPALI@users.noreply.github.com>
2026-06-04 11:01:08 +0000

    work on job queue work schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -12,7 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-       $schedule->command('queue:work --once')->everyMinute();
+    //    $schedule->command('queue:work --once')->everyMinute();
     }
 
     /**
